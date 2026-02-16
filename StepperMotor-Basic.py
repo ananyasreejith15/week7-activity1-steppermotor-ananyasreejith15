3c0f8dd10286e61ba6e306c8a7aac718d4ab289d
ananyasreejith15 <ananyasreejith07@gmail.com>
2026-02-16 14:37:01 +0530

    Update StepperMotor-Basic.py

--- StepperMotor-Basic.py
+++ StepperMotor-Basic.py
@@ -1,1 +1,37 @@
 #Write your code here to run the stepper motor without using any loop
+from machine import Pin
+import time
+
+led1 = Pin(12, Pin.OUT)
+led2 = Pin(14, Pin.OUT)
+led3 = Pin(26, Pin.OUT)
+led4 = Pin(33, Pin.OUT)
+
+t = 0.1
+
+while True:
+    led1.value(1) 
+    led2.value(0) 
+    led3.value(0) 
+    led4.value(0) 
+    time.sleep(t)
+    
+    led1.value(0)
+    led2.value(1)
+    led3.value(0)
+    led4.value(0)
+    time.sleep(t)
+    
+    led1.value(0)
+    led2.value(0)
+    led3.value(1)
+    led4.value(0)
+    time.sleep(t)
+    
+    led1.value(0)
+    led2.value(0)
+    led3.value(0)
+    led4.value(1)
+    time.sleep(t)
+    
+    
